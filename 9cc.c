f8eefcc671988218e9b585b442aaa15703581cc9
tko-cactus <37374201+tko-cactus@users.noreply.github.com>
2020-04-26 18:04:54 +0900

    add tokenizer(in progress)

--- 9cc.c
+++ 9cc.c
@@ -1,6 +1,55 @@
+#include<ctype.h>
+#include<stdarg.h>
+#include<stdbool.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
+// トークンの種類
+typedef enum {
+	TK_RESERVED,
+	TK_NUM,
+	TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+	TokenKind kind;	// トークンの型
+	Token *next;	// 次の入力トークン
+	int val;		// kindがTK_NUMの場合、その数値
+	char *str;		// トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...) {
+	va_list apl
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+// 次のトークンが期待している記号のときには、トークンを1強み勧めて
+// 心を返す。それ以外の場合には偽を返す。
+bool cousume(char op) {
+	if (token->kind != TK_RESERVED || token->str[0] =! op) {
+		return false;
+	}
+	token = token->next;
+	return true;
+}
+
+// 次のトークンが数値の場合、トークンを1強み勧めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
+int expect_number() {
+	if (token->kind)
+}
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
